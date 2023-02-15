eb96316821f4a6b2cde2892441e1318b79efd038
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2023-02-14 20:09:59 -0500

    [func] modifica o texto do endpoint raiz
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello fucking World!';
+    return '<h1> Biblioteca ExLibris </h1> <h3>Julio Cesar Torres (C) 2022</h3>';
   }
 }
